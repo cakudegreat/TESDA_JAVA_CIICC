f55b5f119fdb9499fc9bdb6b6ca883927bb02f20
cakudegreat <estavillo.200184@caloocan.sti.edu.ph>
2026-06-05 23:23:16 +0800

    assignments

---
+++ src/TaskSheet_1_1_1.java
@@ -0,0 +1,13 @@
+public class TaskSheet_1_1_1 {
+    /**
+     * The HelloWorldApp class implements an application that
+     * simply prints "Hello World!" to standard output.
+     */
+    class helloWorldApp {
+        public static void main(String[] args) {
+            System.out.println("Hello World!"); //Display the string.
+        }
+    }
+}
+
+
